17c145392efdd5f5a6f942e8e19d772c7bdbf4ca
yukicoding <hyq1046410512@gmail.com>
2021-10-12 18:30:04 +0800

    factor ejs渲染模板

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,29 @@
-const router = require('koa-router')()
+const Router = require('koa-router')
+const router = Router()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    msg:'你好啊',
+    isMe:false,
+    blogList:[
+      {
+        id:1,
+        title:'aaa'
+      },
+      {
+        id:2,
+        title:'bbb'
+      },
+      {
+        id:3,
+        title:'cccc'
+      },
+      {
+        id:4,
+        title:'dddd'
+      }
+    ]
   })
 })
 
@@ -13,6 +34,22 @@ router.get('/string', async (ctx, next) => {
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/load/:username',async (ctx,next)=>{
+  const {username} = ctx.params
+  ctx.body = {
+    title:'this is a username',
+    username
+  }
+})
+
+router.post('/load',async (ctx,next)=>{
+  const {username,password} = ctx.request.body;
+  ctx.body = {
+    username,
+    password
   }
 })
 
